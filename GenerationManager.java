292476b87a0bb27fdcdc34a9a0372f01c301eff5
Brian Hulbert <Brian.hulbert@me.com>
2018-12-12 09:04:21 -0500

    Further work on generation crossover

--- GenerationManager.java
+++ GenerationManager.java
@@ -13,13 +13,44 @@ public class GenerationManager{
             Generation.add(new NeuralNetwork(struct));
         }
     }
+    public void sortGen(){
+        ArrayList<NeuralNetwork> newGen = new ArrayList<NeuralNetwork>();
+        newGen.add(Generation.get(0));
+        for(int a = 1; a < Generation.size();a++){
+            for(int b = 0; b < newGen.size();b++){
+                if(Generation.get(a).getFitness() > newGen.get(b).getFitness()){
+                    newGen.add(b,Generation.get(a));
+                }
+            }
+        }
+        Generation = newGen;
+    }
     public void crossGeneration(){
         ArrayList<NeuralNetwork> newGen = new ArrayList<NeuralNetwork>();
-        for(int i = 0; i < 2; i++){
+        int numNewNetworks = 2;
+        for(int i = 0; i < numNewNetworks; i++){
             newGen.add(new NeuralNetwork(struct));
         }
-        for(int i = 0; i < 48; i++){
-            
+        for(int i = 0; i < Generation.size()-numNewNetworks; i++){
+           newGen.add(cross(Generation.get(i%10),Generation.get((int)(Math.random()*Generation.size())+1)));
         }
+        Generation = newGen;
+    }
+    public NeuralNetwork cross(NeuralNetwork x, NeuralNetwork y){
+        NeuralNetwork temp = new NeuralNetwork(struct);
+        ArrayList<double[][]> w = temp.getWeights();
+        for(int a = 0; a < w.size();a++){
+            for(int b = 0; b < w.get(a).length;b++){
+                for(int c = 0; c < w.get(a)[b].length;c++){
+                    if(Math.random() > 0.5){
+                        w.get(a)[b][c] = x.getWeights().get(a)[b][c];
+                    }
+                    else{
+                        w.get(a)[b][c] = y.getWeights().get(a)[b][c];
+                    }
+                }
+            }
+        }
+        return temp;
     }
 }
