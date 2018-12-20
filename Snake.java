3833e393bb9396313297b951afa5cb563a40e869
Brian Hulbert <Brian.hulbert@me.com>
2018-12-20 09:18:14 -0500

    further work on the snake game

--- Snake.java
+++ Snake.java
@@ -13,16 +13,84 @@ public class Snake{
         s.add(start);
         foodCords = new int[2];
         randFoodCord();
-        direction = Math.random();
+        //0 == up, 1 == right, 2 == down, 3 = left
+        direction = (int)(Math.random()*4);
     }
     public void randFoodCord(){
         foodCords[0] = (int)(Math.random()*20+1);
         foodCords[1] = (int)(Math.random()*20+1);
     }
+    public boolean tailContains(int x,int y){
+        for(int i = 0; i < s.size(); i++){
+            if(s.get(i)[0] == x && s.get(i)[1] == y){
+                return true;
+            }
+        }
+        return false;
+    }
+    public double[] getInputs(){
+        double[] ins = new double[8];
+        if(s.get(0)[1]-1 <= 0 || tailContains(s.get(0)[0],s.get(0)[1]-1)){
+            ins[0] = 1;
+        }
+        else{
+            ins[0] = 0;
+        }
+        if(s.get(0)[1]+1 >= 20 || tailContains(s.get(0)[0]+1,s.get(0)[1])){
+            ins[1] = 1;
+        }
+        else{
+            ins[1] = 0;
+        }
+        if(s.get(0)[1]+1 <= 0 || tailContains(s.get(0)[0],s.get(0)[1]+1)){
+            ins[2] = 1;
+        }
+        else{
+            ins[2] = 0;
+        }
+        if(s.get(0)[1]-1 >= 20 || tailContains(s.get(0)[0]-1,s.get(0)[1])){
+            ins[3] = 1;
+        }
+        else{
+            ins[3] = 0;
+        }
+        ins[4] = s.get(0)[0]/20;
+        ins[5] = s.get(0)[1]/20;
+        ins[6] = foodCords[0]/20;
+        ins[7] = foodCords[1]/20;
+        return ins;
+    }
+    public int getChoice(double[] vals){
+        int largest = 0;
+        for(int i = 0; i < vals.length;i++){
+            if(vals[i] > vals[largest]){
+                largest = i;
+            }
+        }
+        return largest;
+    }
+    public void move(int x,int y){
+        for(int i = 1;i < s.size();i++){
+            s.set(i,s.get(i-1));
+        }
+        s.get(0)[0] = x;
+        s.get(0)[1] = y;
+    }
     public void simulate(NeuralNetwork NN){
         boolean contin = true;
         while(contin){
-            
+            int choice = getChoice(NN.forwardPropagate(getInputs()));
+            if(choice == 0){
+                move(s.get(0)[0],s.get(0)[1]-1);
+            }
+            else if(choice == 1){
+                move(s.get(0)[0]+1,s.get(0)[1]);
+            }
+            else if(choice == 2){
+                move(s.get(0)[0],s.get(0)[1]+1);
+            }
+            else{
+                move(s.get(0)[0]-1,s.get(0)[1]);
+            }
         }
     }
-}
